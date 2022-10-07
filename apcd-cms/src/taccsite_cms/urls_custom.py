ec38c70756c97fb1b6cc2a98c02acdf6e66aee1a
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2022-10-07 12:54:23 -0500

    fix: use correct url for form

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 custom_urls = [
     path('custom_example/', include('apps.custom_example.urls', namespace='custom_example')),
-    path('apcd/', include('apps.submission_form.urls', namespace='apcd'))
+    path('register/request-to-submit/', include('apps.submission_form.urls', namespace='apcd'))
 ]
