dcdce0bf169e581db60776bcc94caf3c300ba1f5
Strmiska <keith.strmiska@gmail.com>
2022-10-07 12:32:45 -0500

    form fixes for requirements and data types

--- apcd-cms/src/apps/submission_form/views.py
+++ apcd-cms/src/apps/submission_form/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views.generic import View
 from apps.submission_form import apcd_database
@@ -7,14 +8,18 @@ import json
 
 logger = logging.getLogger(__name__)
 
+
 class SubmissionFormView(View):
     def get(self, request):
-        template = loader.get_template('submission_form/submission_form.test.html')
-        return HttpResponse(template.render({}, request))
+        user = authenticate(request)
+        if user:
+            template = loader.get_template('submission_form/submission_form.html')
+            return HttpResponse(template.render({}, request))
+        else:
+            return HttpResponseRedirect('/')
     
     def post(self, request):
         form = request.POST.copy()
-        print(json.dumps(form))
 
         reg_id = apcd_database.create_registration(form)
         for iteration in range(1,6):
