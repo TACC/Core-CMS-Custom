b3c1f420ef9c4a3e64e0934c23feb4f4852883cd
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-10-11 12:07:29 -0500

    test(apcd-cms): FP-1894 custom form at temp URL
    This URL change allows us to:
    - test (i.e. compare) the form in prod
    - load placeholder at final URL

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 
 custom_urls = [
     path('custom_example/', include('apps.custom_example.urls', namespace='custom_example')),
-    path('register/request-to-submit/', include('apps.submission_form.urls', namespace='apcd'))
+    # FP-1894: Do not support custom form at final URL yet
+    # path('register/request-to-submit/', include('apps.submission_form.urls', namespace='apcd'))
+    path('register/request-to-submit-form/', include('apps.submission_form.urls', namespace='apcd'))
 ]
