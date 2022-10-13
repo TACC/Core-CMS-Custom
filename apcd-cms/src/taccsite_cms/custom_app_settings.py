6d9b0b1faa07c951df418b25a8c47fd328f056ba
Keith G Strmiska <keith.strmiska@gmail.com>
2022-10-13 12:22:13 -0500

    Task/fp 1862  configure apcd database client (#14)
    * add database queries and create custom form views
    
    * get form submitting
    
    * form fixes for requirements and data types
    
    * fix: use correct url for form
    
    * Revert "Merge branch 'epic/apcd-qa-2022-Q3' into task/FP-1862--configure-apcd-database-client"
    
    This reverts commit db4cd5ef0cb9b7ca7c638370f768397887ad8123, reversing
    changes made to ec38c70756c97fb1b6cc2a98c02acdf6e66aee1a.
    
    * fix submission method selection
    
    * fix: add step for all "Total Covered Lives" inputs
    
    * remove additional contacts and entities
    
    * remove iteration in view
    
    * convert strings to ints
    
    * logging for debug
    
    * fix: smaller value for org type
    
    * test: remove print statements
    
    * fix: readonly enrollment checkbox
    
    * Revert "test: remove print statements"
    
    This reverts commit 7e5a8b4fd721467047afbc34b91b8695affbe555.
    
    * fix: disabled enrollment checkbox
    
    * Revert "Revert "test: remove print statements""
    
    This reverts commit b0a5309bbe0c90b0930748edb2e55c5dba91bb9f.
    
    Co-authored-by: Wesley B <62723358+wesleyboar@users.noreply.github.com>
    Co-authored-by: Wesley Bomar <wbomar@tacc.utexas.edu>

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,3 +1,3 @@
-CUSTOM_APPS = []
+CUSTOM_APPS = ['apps.custom_example', 'apps.submission_form']
 CUSTOM_MIDDLEWARE = []
 STATICFILES_DIRS = ('taccsite_custom/apcd-cms',)
--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -1,3 +1,6 @@
 from django.urls import path, include
 
-custom_urls = []
+custom_urls = [
+    path('custom_example/', include('apps.custom_example.urls', namespace='custom_example')),
+    path('register/request-to-submit/', include('apps.submission_form.urls', namespace='apcd'))
+]
