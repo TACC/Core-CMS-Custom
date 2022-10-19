80c932d34d5058e146048850e9d6a51f0109b286
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-10-19 13:42:07 -0500

    fix(apcd-cms): do not load custom app

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 
 custom_urls = [
-    path('custom_example/', include('apps.custom_example.urls', namespace='custom_example')),
     path('register/request-to-submit/', include('apps.submission_form.urls', namespace='apcd'))
 ]
