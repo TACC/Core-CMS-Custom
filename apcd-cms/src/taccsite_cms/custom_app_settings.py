992e86ada02724553e2358d18c2e57f9022eb49a
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-10-24 17:17:06 -0500

    CSS cleanup: moved modal css and table css into separate stylesheets, linked to those stylesheets, and updated STATICFILES_DIRS

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,3 +1,3 @@
 CUSTOM_APPS = ['apps.admin_regis_table', 'apps.submission_form']
 CUSTOM_MIDDLEWARE = []
-STATICFILES_DIRS = ('taccsite_custom/apcd-cms',)
+STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/admin_regis_table',)
