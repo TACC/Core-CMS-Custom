fd74ce30dc7be506977bcb39cc5d4ad19ca5a720
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-10-29 22:30:06 -0500

    feat(style-cms): fp-1500 minimal solution
    - Load at http://localhost:8000/pattern-library/index.html.
    - Abandon using CMS header (seems impossible).

--- style-cms/src/apps/pattern_library/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PatternLibraryConfig(AppConfig):
-    name = 'pattern_library'
--- style-cms/src/taccsite_cms/custom_app_settings.py
+++ style-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,6 +1,3 @@
-CUSTOM_APPS = ['apps.pattern_library']
+CUSTOM_APPS = []
 CUSTOM_MIDDLEWARE = []
-# Serve pattern library static files without header
-# FAQ: How to serve static files the prod-ready way
-# STATICFILES_DIRS = (('pattern_library', 'taccsite_ui/dist'), 'apps/pattern_library')
-STATICFILES_DIRS = ('taccsite_custom/styles-cms', 'apps/pattern_library')
+STATICFILES_DIRS = ()
