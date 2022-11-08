b7e6b67c59d759dc718be140283f33bcbb5814eb
Strmiska <keith.strmiska@gmail.com>
2022-11-08 16:48:35 -0600

    update login url

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 custom_urls = [
     path('administration/', include('apps.admin_regis_table.urls', namespace='administration')),
-    path('login/', include('apps.login.urls', namespace='login')),
+    path('apcd-login/', include('apps.apcd_login.urls', namespace='apcd_login')),
     path('register/', include('apps.registrations.urls', namespace='register'))
 ]
