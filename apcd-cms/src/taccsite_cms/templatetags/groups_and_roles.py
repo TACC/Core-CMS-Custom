e59a118bac6e02335e755e7017e3995b0e310347
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-11-10 15:55:08 -0600

    feat(apcd-cms): fp-1837 nav item limit poc cleaner
    TO DO:
    1. Clone Core CMS nav menu markup.
    2. Use new logic within that cloned markup.
    3. Migrate to Core CMS:
        - nav can come as is
        - template tag need always return true

--- apcd-cms/src/taccsite_cms/templatetags/groups_and_roles.py
+++ apcd-cms/src/taccsite_cms/templatetags/groups_and_roles.py
@@ -5,21 +5,26 @@ from django.contrib import auth as auth
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def has_apcd_group(context):
-    request = context['request']
-    user = request.user
-
+def has_apcd_group(user):
     for group in user.groups.all():
         if group.name in ['APCD_ADMIN', 'SUBMITTER_ADMIN', 'SUBMITTER_USER']:
             return True
-
     return False
 
-@register.simple_tag(takes_context=True)
-def is_apcd_admin(context):
-    request = context['request']
-    user = request.user
-
+def is_apcd_admin(user):
     return user.groups.filter(name='APCD_ADMIN').exists()
 
+@register.simple_tag(takes_context=True)
+def limit_visibility_in_menu(context, menu_item):
+    request = context['request']
+    user = request.user
+    page_id = menu_item.attr['reverse_id']
+
+    if (
+        page_id == 'submissions' and has_apcd_group(user) or
+        page_id == 'administration' and is_apcd_admin(user) or
+        (not page_id == 'submissions' and not page_id == 'administration')
+    ):
+        return True
+    else:
+        return False
