375d1b8b758b4c7fa4d6cdb34693dc7794f48ffd
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2022-11-10 17:34:14 -0600

    test(apcd-cms): fp-1837 limit menu visibility by group (#45)
    * test(apcd-cms): fp-1837 overwrite cms nav poc
    
    * feat(apcd-cms): fp-1837 logic (not working)
    
    * feat(apcd-cms): fp-1837 nav item limit poc working
    
    Limit what nav items are shown to user based on their group.
    
    There should be a more parametric way to do this, so we can:
    - integrate this into core
    - rely on CMS page group perms, not code
    
    * feat(apcd-cms): fp-1837 nav item limit poc cleaner
    
    TO DO:
    1. Clone Core CMS nav menu markup.
    2. Use new logic within that cloned markup.
    3. Migrate to Core CMS:
        - nav can come as is
        - template tag need always return true
    
    * feat(apcd-cms): fp-1837 rename template tag
    
    TO DO:
    1. Clone Core CMS nav menu markup.
    2. Use new logic within that cloned markup.
    3. Migrate to Core CMS:
        - nav can come as is
        - template tag need always return true
    
    * feat(apcd-cms): fp-1837 clone core cms nav markup
    
    TO DO:
    1. [x] Clone Core CMS nav menu markup.
    2. [x] Use new logic within that cloned markup.
    3. Migrate to Core CMS:
        - nav can come as is
        - template tag need always return true
    
    * docs(apcd-cms): fp-1837 explain, remove dup code

---
+++ apcd-cms/src/taccsite_cms/templatetags/limit_visibility_in_menu.py
@@ -0,0 +1,49 @@
+from django import template
+from urllib.parse import urlparse
+from django.utils.html import format_html
+from django.contrib import auth as auth
+
+register = template.Library()
+
+def has_apcd_group(user):
+    for group in user.groups.all():
+        if group.name in ['APCD_ADMIN', 'SUBMITTER_ADMIN', 'SUBMITTER_USER']:
+            return True
+    return False
+
+def is_apcd_admin(user):
+    return user.groups.filter(name='APCD_ADMIN').exists()
+
+@register.simple_tag(takes_context=True)
+def limit_visibility_in_menu(context, menu_item):
+    """
+    Custom Template Tag `limit_visibility_in_menu`
+
+    Use: Return (boolean) whether given menu item is visible by current user.
+
+    Load custom tag into template:
+        {% load limit_visibility_in_menu %}
+
+    Template inline usage:
+        {# (renders `True` or `False`) #}
+        {% limit_visibility_in_menu menu_item %}
+
+        {# (renders "A" or "B") #}
+        {% limit_visibility_in_menu menu_item as can_view %}
+        {% if can_view %} A {% else %} B {% endif %}
+
+    Example:
+        ../templates/cms_menu.html
+    """
+    request = context['request']
+    user = request.user
+    page_id = menu_item.attr['reverse_id']
+
+    if (
+        page_id == 'submissions' and has_apcd_group(user) or
+        page_id == 'administration' and is_apcd_admin(user) or
+        (not page_id == 'submissions' and not page_id == 'administration')
+    ):
+        return True
+    else:
+        return False
