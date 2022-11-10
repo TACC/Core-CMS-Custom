c8eabb20b544916ae38ea6ba580d636eab2b5c0f
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-11-10 16:26:31 -0600

    docs(apcd-cms): fp-1837 explain, remove dup code

--- apcd-cms/src/taccsite_cms/templatetags/limit_visibility_in_menu.py
+++ apcd-cms/src/taccsite_cms/templatetags/limit_visibility_in_menu.py
@@ -16,6 +16,25 @@ def is_apcd_admin(user):
 
 @register.simple_tag(takes_context=True)
 def limit_visibility_in_menu(context, menu_item):
+    """
+    Custom Template Tag `limit_visibility_in_menu`
+
+    Use: Return (boolean) whether given menu item is visible by current user.
+
+    Load custom tag into template:
+        {% load limit_visibility_in_menu %}
+
+    Template inline usage:
+        {# (renders `True` or `False`) #}
+        {% limit_visibility_in_menu menu_item %}
+
+        {# (renders "A" or "B") #}
+        {% limit_visibility_in_menu menu_item as can_view %}
+        {% if can_view %} A {% else %} B {% endif %}
+
+    Example:
+        ../templates/cms_menu.html
+    """
     request = context['request']
     user = request.user
     page_id = menu_item.attr['reverse_id']
