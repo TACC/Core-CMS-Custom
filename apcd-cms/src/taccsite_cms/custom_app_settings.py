c5594316df61e37de283d69cc82da544b3a0a665
Shayan Khan <shayanaijaz@gmail.com>
2023-01-17 14:17:29 -0600

    added list submissions page, and database calls to get submission data (#63)

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,3 +1,3 @@
-CUSTOM_APPS = ['apps.admin_regis_table', 'apps.apcd_login', 'apps.registrations']
+CUSTOM_APPS = ['apps.admin_regis_table', 'apps.apcd_login', 'apps.registrations', 'apps.submissions']
 CUSTOM_MIDDLEWARE = []
-STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/admin_regis_table',)
+STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/admin_regis_table', 'apps/submissions')
