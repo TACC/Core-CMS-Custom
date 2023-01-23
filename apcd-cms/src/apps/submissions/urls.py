a709203a61ac136066dca9265e4887a53d00accc
Sophia Massie <smmassie92@gmail.com>
2023-01-23 15:38:04 -0600

    -limit date and threshold requested field -add css

--- apcd-cms/src/apps/submissions/urls.py
+++ apcd-cms/src/apps/submissions/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from apps.submissions.views import SubmissionsTable
 from apps.admin_regis_table.urls import RegistrationsTable
+from apps.exception.views import ExceptionFormView, ExceptionThresholdFormView, ExceptionOtherFormView
 
 app_name = 'submissions'
 urlpatterns = [
     path('list-submissions/', SubmissionsTable.as_view()),
+    path('exception/', ExceptionFormView.as_view(), name="exception"),
+    path('threshold-exception/', ExceptionThresholdFormView.as_view(), name="threshold-exception"),
+    path('other-exception/', ExceptionOtherFormView.as_view(), name="other-exception")
 ]
