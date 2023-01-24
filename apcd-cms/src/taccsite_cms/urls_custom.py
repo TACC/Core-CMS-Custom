e12b6323e997ace21a4685416e4f5f10db24a06e
Sophia Massie <smmassie92@gmail.com>
2023-01-24 12:28:48 -0600

    Create styles, setup form, create submission path

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -5,6 +5,6 @@ custom_urls = [
     path('administration/', include('apps.admin_submissions.urls', namespace='admin_submission')),
     path('apcd-login/', include('apps.apcd_login.urls', namespace='apcd_login')),
     path('register/', include('apps.registrations.urls', namespace='register')),
-    path('extension/', include('apps.extension.urls', namespace='extension'))
+    path('submissions/', include('apps.extension.urls', namespace='extension')),
     path('submissions/', include('apps.submissions.urls', namespace='submissions'))
 ]
