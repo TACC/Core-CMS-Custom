723fbb99f18dde3baa50282a52de48abe662a426
davidmtacc <dmontoya@tacc.utexas.edu>
2023-01-24 08:33:14 -0800

    Created View User app
    List users

---
+++ apcd-cms/src/apps/view_users/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ViewUsersConfig(AppConfig):
+    name = 'view_users'
--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 custom_urls = [
     path('administration/', include('apps.admin_regis_table.urls', namespace='administration')),
+    path('administration/', include('apps.view_users.urls', namespace='viewusers')),
     path('apcd-login/', include('apps.apcd_login.urls', namespace='apcd_login')),
     path('register/', include('apps.registrations.urls', namespace='register'))
 ]
