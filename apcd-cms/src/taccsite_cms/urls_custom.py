7ed78764a986c9c57425598005bd097400285280
Sophia Massie <smmassie92@gmail.com>
2023-01-24 08:05:29 -0600

    Added style, adjusted url + app custom_app_setting

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -4,6 +4,6 @@ custom_urls = [
     path('administration/', include('apps.admin_regis_table.urls', namespace='administration')),
     path('apcd-login/', include('apps.apcd_login.urls', namespace='apcd_login')),
     path('register/', include('apps.registrations.urls', namespace='register')),
-    path('exception/', include('apps.exception.urls', namespace='exception')),
+    path('submissions/', include('apps.exception.urls', namespace='exception')),
     path('submissions/', include('apps.submissions.urls', namespace='submissions'))
 ]
