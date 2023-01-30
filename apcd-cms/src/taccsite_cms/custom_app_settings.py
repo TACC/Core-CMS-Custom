cc3c1ef757f56e3add6292d33b650670c2d54807
shayanaijaz <shayanaijaz@gmail.com>
2023-01-30 10:33:08 -0600

    added view extensions page

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,3 +1,3 @@
-CUSTOM_APPS = ['apps.admin_regis_table', 'apps.apcd_login', 'apps.registrations', 'apps.submissions', 'apps.admin_submissions']
+CUSTOM_APPS = ['apps.admin_regis_table', 'apps.apcd_login', 'apps.registrations', 'apps.submissions', 'apps.admin_submissions', 'apps.admin_extension']
 CUSTOM_MIDDLEWARE = []
 STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/admin_regis_table', 'apps/submissions')
