eb7eb7a41169a73b2cedf1ba33a3b3ab7bb6aea7
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-01-30 16:57:35 -0600

    task/FP-1816-Extension-Request (#75)
    * Working off clean repo - modified view
    
    * Update path in custom_app_settings and urls_custom

--- apcd-cms/src/apps/utils/apcd_database.py
+++ apcd-cms/src/apps/utils/apcd_database.py
@@ -880,6 +880,127 @@ def get_all_submissions():
         if conn is not None:
             conn.close()
 
+def create_extension(form, iteration, sub_data):
+    cur = None
+    conn = None
+    values = ()
+    try:
+        if iteration >= 1:
+            values = (
+                None,
+                _clean_value(sub_data[0]),
+                _clean_value(form['current-expected-date_{}'.format(iteration)]),
+                _clean_value(form['requested-target-date_{}'.format(iteration)]),
+                None,
+                _clean_value(form['extension-type_{}'.format(iteration)]),
+                _clean_value(form['applicable-data-period_{}'.format(iteration)]),
+                "Pending",
+                None,
+                datetime.datetime.now(),
+                None,
+                _clean_value(sub_data[1]),
+                _clean_value(sub_data[2]),
+                _clean_value(sub_data[3]),
+                _clean_value(form["requestor-name"]),
+                _clean_email(form["requestor-email"]),
+                _clean_value(form["justification"]),
+                None
+                )
+        else:
+            values = (
+            None,
+            _clean_value(sub_data[0]),
+            _clean_value(form['current-expected-date']),
+            _clean_value(form['requested-target-date']),
+            None,
+            _clean_value(form['extension-type']),
+            _clean_value(form['applicable-data-period']),
+            "Pending",
+            None,
+            datetime.datetime.now(),
+            None,
+            _clean_value(sub_data[1]),
+            _clean_value(sub_data[2]),
+            _clean_value(sub_data[3]),
+            _clean_value(form["requestor-name"]),
+            _clean_email(form["requestor-email"]),
+            _clean_value(form["justification"]),
+            None
+            )            
+            operation = """INSERT INTO extensions(
+                    extension_id,
+                    submitter_id,
+                    current_expected_date,
+                    requested_target_date,
+                    approved_expiration_date,
+                    extension_type,
+                    applicable_data_period,
+                    status,
+                    outcome,
+                    created_at,
+                    updated_at,
+                    submitter_code,
+                    payor_code,
+                    user_id,
+                    requestor_name,
+                    requestor_email,
+                    explanation_justification,
+                    notes
+                    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+            conn = psycopg2.connect(
+                host=APCD_DB['host'],
+                dbname=APCD_DB['database'],
+                user=APCD_DB['user'],
+                password=APCD_DB['password'],
+                port=APCD_DB['port'],
+                sslmode='require'
+            )
+            cur = conn.cursor()
+            cur.execute(operation, values)
+            conn.commit()
+    except Exception as error:
+        logger.error(error)
+        return error
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+def get_submitter_for_extend_or_except():
+    cur = None
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host=APCD_DB["host"],
+            dbname=APCD_DB["database"],
+            user=APCD_DB["user"],
+            password=APCD_DB["password"],
+            port=APCD_DB["port"],
+            sslmode="require",
+        )
+        user = APCD_DB["user"]
+        cur = conn.cursor()
+        query = """SELECT submitters.submitter_id, submitters.submitter_code, submitters.payor_code, submitter_users.user_id 
+        FROM submitters 
+        LEFT JOIN submitter_users 
+        ON submitters.submitter_id = submitter_users.submitter_id 
+        WHERE user_id = (%s) 
+        """
+        cur = conn.cursor()
+        cur.execute(query, (user,))
+        return cur.fetchall()
+
+    except Exception as error:
+        logger.error(error)
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
 def get_all_exceptions():
     cur = None
     conn = None
