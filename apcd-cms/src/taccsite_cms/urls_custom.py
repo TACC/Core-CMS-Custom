57b940d323f127c3e6e272fbd800e57ffac69be8
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-01-30 11:45:02 -0600

    task/FP 1815 exception page.1/23 (#72)
    * Page format - need to fix views and update queries
    
    * Updated date fields to match
    
    * Update dates
    
    * Initial layout like PDF - copied from extension
    
    * Added threshold fields
    
    * Added fields for thresholds
    
    * Need to make other option remove threshold fields
    
    * WIP
    
    * TODO update view and main page submit button
    
    * Front end renders pages correctly Views need setup
    
    * Merge conflicts updated
    
    * Changing Name of Other Excp Page
    
    * Updated comment
    
    * Updated comments
    
    * Fixed hanging div
    
    * Formatting templates
    
    * Update apcd-cms/src/apps/exception/templates/exception_submission_form/exception_other_form.html
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    
    * Update apcd-cms/src/apps/exception/templates/exception_submission_form/exception_other_form.html
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    
    * Update apcd-cms/src/taccsite_cms/urls_custom.py
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    
    * WIP merged main need view to populate fields
    
    * Updated field code to input not select
    
    * Update
    
    * Works locally, formatted, small changes
    
    * Changed title landing Page
    
    * Adjusting footnotes
    
    * added admin submissions page (#68)
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    
    * Added time restriction no past and not past 1 year
    
    * -limit date and threshold requested field -add css
    
    * Moved security/role check to top of view function
    
    * fix for navigating to page without page number (#70)
    
    * Handling for a few NoneType values in database in registrations listing (#71)
    
    * Added style, adjusted url + app custom_app_setting
    
    * Merging conflicts
    
    * Got rid of extra code and paths
    
    * Fixed huge diff in apcd_database and merge error
    
    * Fixing diff in apcd_database
    
    * Fixed huge diff in apcd_database
    
    * Fixing diff in apcd_database
    
    * Fixing diff in apcd_database
    
    * Fixing diff in apcd_database
    
    * Update apcd-cms/src/apps/submissions/urls.py
    
    Co-authored-by: Shayan Khan <shayanaijaz@gmail.com>
    
    * Adjusted urls file to remove extra code
    
    ---------
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    Co-authored-by: Shayan Khan <shayanaijaz@gmail.com>

--- apcd-cms/src/taccsite_cms/urls_custom.py
+++ apcd-cms/src/taccsite_cms/urls_custom.py
@@ -5,5 +5,6 @@ custom_urls = [
     path('administration/', include('apps.admin_submissions.urls', namespace='admin_submission')),
     path('apcd-login/', include('apps.apcd_login.urls', namespace='apcd_login')),
     path('register/', include('apps.registrations.urls', namespace='register')),
+    path('submissions/', include('apps.exception.urls', namespace='exception')),
     path('submissions/', include('apps.submissions.urls', namespace='submissions'))
 ]
