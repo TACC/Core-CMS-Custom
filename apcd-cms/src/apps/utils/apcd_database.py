1f3142b135ca8b466c073f8406289a7ababfa7b3
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-02-03 13:33:47 -0600

    Task/fp 1816 extension requeste (#82)
    * Working off clean repo - modified view
    
    * Update path in custom_app_settings and urls_custom
    
    * Updated create_extension util -removed RT ticket creation
    
    * modified query to get user org names
    
    * added dynamic dropdown for org selection, fixed data format for db field
    
    * Added date check and iteration check for input
    
    * Fixed clean_date util in apcd_database
    
    * fix for additional extensions not appearing, cleaned up html
    
    * fix typo
    
    * Update apcd-cms/src/apps/extension/templates/extension_submission_form/extension_submission_error.html
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    
    * Fixing merge error
    
    ---------
    
    Co-authored-by: shayanaijaz <shayanaijaz@gmail.com>
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>

--- apcd-cms/src/apps/utils/apcd_database.py
+++ apcd-cms/src/apps/utils/apcd_database.py
@@ -969,24 +969,23 @@ def get_all_extensions():
         if conn is not None:
             conn.close()
 
-
 def create_extension(form, iteration, sub_data):
     cur = None
     conn = None
     values = ()
     try:
-        if iteration >= 1:
+        if iteration > 1:
             values = (
                 None,
                 _clean_value(sub_data[0]),
-                _clean_value(form['current-expected-date_{}'.format(iteration)]),
-                _clean_value(form['requested-target-date_{}'.format(iteration)]),
+                _clean_date(form['current-expected-date_{}'.format(iteration)]),
+                _clean_date(form['requested-target-date_{}'.format(iteration)]),
                 None,
                 _clean_value(form['extension-type_{}'.format(iteration)]),
-                _clean_value(form['applicable-data-period_{}'.format(iteration)]),
+                int(form['applicable-data-period_{}'.format(iteration)].replace('-', '')),
                 "Pending",
                 None,
-                datetime.datetime.now(),
+                datetime.datetime.now().strftime('%Y-%m-%d'),
                 None,
                 _clean_value(sub_data[1]),
                 _clean_value(sub_data[2]),
@@ -1000,14 +999,14 @@ def create_extension(form, iteration, sub_data):
             values = (
             None,
             _clean_value(sub_data[0]),
-            _clean_value(form['current-expected-date']),
-            _clean_value(form['requested-target-date']),
+            _clean_date(form['current-expected-date']),
+            _clean_date(form['requested-target-date']),
             None,
             _clean_value(form['extension-type']),
-            _clean_value(form['applicable-data-period']),
+            int(form['applicable-data-period'].replace('-', '')),
             "Pending",
             None,
-            datetime.datetime.now(),
+            datetime.datetime.now().strftime('%Y-%m-%d'),
             None,
             _clean_value(sub_data[1]),
             _clean_value(sub_data[2]),
@@ -1017,37 +1016,37 @@ def create_extension(form, iteration, sub_data):
             _clean_value(form["justification"]),
             None
             )            
-            operation = """INSERT INTO extensions(
-                    extension_id,
-                    submitter_id,
-                    current_expected_date,
-                    requested_target_date,
-                    approved_expiration_date,
-                    extension_type,
-                    applicable_data_period,
-                    status,
-                    outcome,
-                    created_at,
-                    updated_at,
-                    submitter_code,
-                    payor_code,
-                    user_id,
-                    requestor_name,
-                    requestor_email,
-                    explanation_justification,
-                    notes
-                    ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-            conn = psycopg2.connect(
-                host=APCD_DB['host'],
-                dbname=APCD_DB['database'],
-                user=APCD_DB['user'],
-                password=APCD_DB['password'],
-                port=APCD_DB['port'],
-                sslmode='require'
-            )
-            cur = conn.cursor()
-            cur.execute(operation, values)
-            conn.commit()
+        operation = """INSERT INTO extensions(
+                extension_id,
+                submitter_id,
+                current_expected_date,
+                requested_target_date,
+                approved_expiration_date,
+                extension_type,
+                applicable_data_period,
+                status,
+                outcome,
+                created_at,
+                updated_at,
+                submitter_code,
+                payor_code,
+                user_id,
+                requestor_name,
+                requestor_email,
+                explanation_justification,
+                notes
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+        conn = psycopg2.connect(
+            host=APCD_DB['host'],
+            dbname=APCD_DB['database'],
+            user=APCD_DB['user'],
+            password=APCD_DB['password'],
+            port=APCD_DB['port'],
+            sslmode='require'
+        )
+        cur = conn.cursor()
+        cur.execute(operation, values)
+        conn.commit()
     except Exception as error:
         logger.error(error)
         return error
@@ -1058,32 +1057,84 @@ def create_extension(form, iteration, sub_data):
         if conn is not None:
             conn.close()
 
-def get_submitter_for_extend_or_except():
+def get_submitter_for_extend_or_except(user):
     cur = None
     conn = None
     try:
         conn = psycopg2.connect(
-            host=APCD_DB["host"],
-            dbname=APCD_DB["database"],
-            user=APCD_DB["user"],
-            password=APCD_DB["password"],
-            port=APCD_DB["port"],
-            sslmode="require",
+            host=APCD_DB['host'],
+            dbname=APCD_DB['database'],
+            user=APCD_DB['user'],
+            password=APCD_DB['password'],
+            port=APCD_DB['port'],
+            sslmode='require',
         )
-        user = APCD_DB["user"]
         cur = conn.cursor()
-        query = """SELECT submitters.submitter_id, submitters.submitter_code, submitters.payor_code, submitter_users.user_id 
-        FROM submitters 
-        LEFT JOIN submitter_users 
-        ON submitters.submitter_id = submitter_users.submitter_id 
-        WHERE user_id = (%s) 
-        """
+        query = """
+                SELECT submitters.submitter_id, submitters.submitter_code, submitters.payor_code, submitter_users.user_id, users.org_name
+                FROM submitters
+                JOIN submitter_users
+                    ON submitters.submitter_id = submitter_users.submitter_id
+                JOIN users
+                    ON submitter_users.user_id = users.user_id
+                WHERE submitter_users.user_id = (%s)
+            """
         cur = conn.cursor()
         cur.execute(query, (user,))
         return cur.fetchall()
 
     except Exception as error:
         logger.error(error)
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+
+def get_all_extensions():
+    cur = None
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host=APCD_DB['host'],
+            dbname=APCD_DB['database'],
+            user=APCD_DB['user'],
+            password=APCD_DB['password'],
+            port=APCD_DB['port'],
+            sslmode='require'
+        )
+        query = """
+            SELECT 
+                extensions.extension_id, 
+                extensions.submitter_id,
+                extensions.current_expected_date,
+                extensions.requested_target_date,
+                extensions.approved_expiration_date,
+                extensions.extension_type,
+                extensions.applicable_data_period,
+                extensions.status,
+                extensions.outcome,
+                extensions.created_at,
+                extensions.updated_at,
+                extensions.submitter_code,
+                extensions.payor_code,
+                extensions.user_id,
+                extensions.requestor_name,
+                extensions.requestor_email,
+                extensions.explanation_justification,
+                extensions.notes,
+                users.org_name
+            FROM extensions
+            JOIN submitter_users
+                ON extensions.submitter_id = submitter_users.submitter_id
+            JOIN users
+                ON submitter_users.user_id = users.user_id
+            ORDER BY extensions.created_at DESC
+        """ 
+        cur = conn.cursor()
+        cur.execute(query)
+        return cur.fetchall()
 
     finally:
         if cur is not None:
@@ -1182,8 +1233,15 @@ def _clean_email(email):
     return result.string if result else None
 
 def _clean_value(value):
-    return re.sub('[^a-zA-Z0-9 \.\-\,]', '', value)
+    return re.sub('[^a-zA-Z0-9 \.\-\,]', '', str(value))
 
+def _clean_date(date_string):
+    date_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+
+    if re.match(date_pattern, date_string):
+        return date_string
+    else:
+        return None
 
 def _set_int(value):
     if len(value):
