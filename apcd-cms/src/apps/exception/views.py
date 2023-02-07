15fc95df12a3c4743ab4bcdb9d7e0f1f3bed899d
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-02-07 10:52:44 -0600

    Update apcd-cms/src/apps/exception/views.py
    Co-authored-by: fnets <fnetscher@tacc.utexas.edu>

--- apcd-cms/src/apps/exception/views.py
+++ apcd-cms/src/apps/exception/views.py
@@ -27,7 +27,6 @@ class ExceptionThresholdFormView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(ExceptionThresholdFormView, self).get_context_data(*args, **kwargs)
 
-        submitters = [(1, 'TESTGOLD', 10000000, 'thbrown', 'chcd'), (4, 'TESTMRTN', 10000003, 'thbrown', 'chcd')]
 
 
         self.request.session['submitters'] = submitters
