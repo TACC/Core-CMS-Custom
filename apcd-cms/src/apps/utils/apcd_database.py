8e30872dc6ddbc742d0c21f79fd8fb752c163d14
davidmtacc <dmontoya@tacc.utexas.edu>
2023-02-08 11:48:40 -0800

    View users (#90)
    * Created View User app
    
    List users
    
    * Uncomment auth lines
    
    Authentication commented out for testing. Removing comments
    
    * Update apcd-cms/src/apps/view_users/static/view_users_table/css/table.css
    
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>
    
    * Update apcd-cms/src/apps/view_users/templates/view_users.html
    
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>
    
    * Fixed FIlter
    
    Added organization dropdown using organizations from table but removing duplicates from dropdown list. Amended db query to orderby orgs
    Fixed column heading Role ID
    
    * Update view_users.html
    
    * Added All
    
    Removed duplicate code and console logs added All option
    
    * Update apcd_database.py
    
    removed old get_users function
    
    * Modified DB function add Role Names
    
    Updated db function to get names for role and to order the results by org_name. Added ALL to dropdown list
    
    * Update apcd-cms/src/apps/view_users/views.py
    
    ---------
    
    Co-authored-by: Shayan Khan <shayanaijaz@gmail.com>
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>

--- apcd-cms/src/apps/utils/apcd_database.py
+++ apcd-cms/src/apps/utils/apcd_database.py
@@ -21,17 +21,7 @@ def get_users():
             port=APCD_DB['port'],
             sslmode='require'
         )
-        query = """SELECT
-                users.user_id,
-                users.user_email,
-                users.user_name,
-                users.org_name,
-                users.role_id,
-                users.created_at,
-                users.updated_at,
-                users.notes
-                FROM users
-                ORDER BY users.org_name ASC
+        query = """SELECT * FROM users NATURAL JOIN roles ORDER BY users.org_name ASC
                 """
         cur = conn.cursor()
         cur.execute(query)
@@ -47,40 +37,6 @@ def get_users():
             conn.close()
             
 
-def get_users():
-    cur = None
-    conn = None
-    try:
-        conn = psycopg2.connect(
-            host=APCD_DB['host'],
-            dbname=APCD_DB['database'],
-            user=APCD_DB['user'],
-            password=APCD_DB['password'],
-            port=APCD_DB['port'],
-            sslmode='require'
-        )
-        query = """SELECT
-                users.user_id,
-                users.user_email,
-                users.user_name,
-                users.org_name,
-                users.role_id,
-                users.created_at,
-                users.updated_at,
-                users.notes
-                FROM users"""
-        cur = conn.cursor()
-        cur.execute(query)
-        return cur.fetchall()
-
-    except Exception as error:
-        logger.error(error)
-    
-    finally:
-        if cur is not None:
-            cur.close()
-        if conn is not None:
-            conn.close()
             
 
 def get_user_role(user):
