be0fba595d954403d00fdc12429dffb3d53bf337
shayanaijaz <shayanaijaz@gmail.com>
2023-02-10 10:37:17 -0600

    clean up

--- apcd-cms/src/apps/admin_extension/views.py
+++ apcd-cms/src/apps/admin_extension/views.py
@@ -12,7 +12,7 @@ import rt
 from apps.utils.apcd_database import get_all_extensions
 from apps.utils.apcd_groups import is_apcd_admin
 from apps.utils.utils import title_case
-import datetime
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -44,7 +44,7 @@ class AdminExtensionsTable(TemplateView):
                 'requested_target_date': extension[3],
                 'approved_expiration_date': extension[4],
                 'extension_type': title_case(extension[5]),
-                'applicable_data_period': datetime.datetime.strptime(str(extension[6]), '%Y%m').strftime('%b. %Y') if extension[6] else None,
+                'applicable_data_period': _get_applicable_data_period(extension[6]),
                 'status': title_case(extension[7]),
                 'outcome': title_case(extension[8]),
                 'created_at': extension[9],
@@ -82,3 +82,10 @@ class AdminExtensionsTable(TemplateView):
         context['num_pages'] = range(1, p.num_pages + 1)
 
         return context
+
+# function converts int value in the format YYYYMM to a string with abbreviated month and year
+def _get_applicable_data_period(value):
+    try: 
+        return datetime.strptime(str(value), '%Y%m').strftime('%b. %Y')
+    except:
+        return None
