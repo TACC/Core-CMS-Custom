864081030aa956b955a96bd34d77b2de9f5dfd87
rstijerina <r.sal.tijerina@gmail.com>
2023-02-21 17:26:46 -0600

    restrict apcd submissions

--- apcd-cms/src/apps/submissions/urls.py
+++ apcd-cms/src/apps/submissions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.submissions.views import SubmissionsTable
+from apps.submissions.views import SubmissionsTable, submit_file_view
 
 app_name = 'submissions'
 urlpatterns = [
     path('list-submissions/', SubmissionsTable.as_view()),
+    path('submit-file/', submit_file_view, name='submit'),
+    # path('submit-file/', SubmissionsView.as_view()),
 ]
--- apcd-cms/src/apps/submissions/views.py
+++ apcd-cms/src/apps/submissions/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.base import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.conf import settings
+from django.shortcuts import redirect, render
+from djangotemplate import Template, Context
 from apps.utils.apcd_database import get_submissions, get_submission_logs
-from apps.utils.apcd_groups import has_apcd_group, is_apcd_admin
+from apps.utils.apcd_groups import has_apcd_group
 from apps.utils.utils import title_case
 import logging
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -48,15 +54,59 @@ class SubmissionsTable(TemplateView):
                     'outcome': title_case(submission_log[5]),
                     'file_type_name': submission_log[6]
                 })
-            
+
             return modal_content
 
 
         context['header'] = ['Received', 'File Name', ' ', 'Outcome', 'Status', 'Last Updated', 'Actions']
-        context['rows'] = [] 
+        context['rows'] = []
 
         for submission in submission_content:
             submission_logs = get_submission_logs(submission[0])
             context['rows'].append(_set_submissions(submission, submission_logs))
 
         return context
+
+
+# @method_decorator(login_required, name='dispatch')
+# class SubmissionsView(TemplateView):
+#     """
+#     Main workbench view.
+#     """
+#     template_name = 'submit_file.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(SubmissionsView, self).get_context_data(**kwargs)
+
+
+#         response = requests.get('{0}/workbench/'.format( \
+#             getattr(settings, 'CEP_AUTH_VERIFICATION_ENDPOINT', 'http://django:6000')),
+#             cookies={'coresessionid': self.request.COOKIES.get('coresessionid')})
+
+#         context['is_submitter'] = True
+#         context['workbench'] = response.text
+
+#         return context
+
+#     def dispatch(self, request, *args, **kwargs):
+
+#         if not has_apcd_group(request.user):
+#             return render(request, 'submit_file_unauthorized.html')
+
+#         return super(SubmissionsView, self).dispatch(request, *args, **kwargs)
+
+@login_required
+def submit_file_view(request):
+
+    if not has_apcd_group(request.user):
+        return render(request, 'submit_file_unauthorized.html')
+
+    response = requests.get('{0}/workbench/'.format( \
+        getattr(settings, 'CEP_AUTH_VERIFICATION_ENDPOINT', 'http://django:6000')),
+        cookies={'coresessionid': request.COOKIES.get('coresessionid')})
+    assert response.status_code == 200
+
+    template = Template(response.content)
+    context = {}
+
+    return HttpResponse(template.render(context, request))
