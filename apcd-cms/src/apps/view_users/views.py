eceb39da607213718dea974cd19a2c878d04ead5
Sophia Massie <smmassie92@gmail.com>
2023-03-13 15:01:30 -0500

    -Added back context['rows'].append(_set_user(user,)) to view_users/views.py so users will be added to context for the view

--- apcd-cms/src/apps/view_users/views.py
+++ apcd-cms/src/apps/view_users/views.py
@@ -3,9 +3,9 @@ from django.views.generic.base import TemplateView
 from apps.utils.apcd_database import get_users
 from apps.utils.apcd_groups import is_apcd_admin
 import logging
+import datetime
 
 logger = logging.getLogger(__name__)
-
 
 class ViewUsersTable(TemplateView):
     template_name = 'view_users.html'
@@ -19,8 +19,6 @@ class ViewUsersTable(TemplateView):
     def get_context_data(self, user_content=user_content, *args, **kwargs):
         context = super(ViewUsersTable, self).get_context_data(*args, **kwargs)
 
-
-
         def _set_user(usr):
             return {
                     'role_id': usr[0],
@@ -33,16 +31,12 @@ class ViewUsersTable(TemplateView):
                     'notes': usr[7],
                     'active': usr[8],
                     'role_name': usr[9],
-    
+
                 }
-
-
-        
-        
 
         context['header'] = ['User ID', 'Email', 'Name', 'Organization', 'Role', 'Active', 'Created', 'Updated', 'Notes']
         context['rows'] = []
         for user in user_content:
-            user_view = [usr for usr in user_content if usr[1] == user[0]]
+            context['rows'].append(_set_user(user,))
 
         return context
