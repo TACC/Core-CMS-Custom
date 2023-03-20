f9d86f5a1782a2c526fc95b54c80d4e8cb670aa4
davidmtacc <dmontoya@tacc.utexas.edu>
2023-03-20 15:08:33 -0700

    modified to allow filter with ampersand and parens (#119)
    * modified to allow filter with ampersand and parens
    
    * removed lines from views.py as suggested by reviewer
    
    * checked if new script worked and reviewed views.py
    
    * previous commit had unsaved views
    
    ---------
    
    Co-authored-by: edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
    Co-authored-by: fnets <fnetscher@tacc.utexas.edu>

--- apcd-cms/src/apps/view_users/views.py
+++ apcd-cms/src/apps/view_users/views.py
@@ -18,6 +18,7 @@ class ViewUsersTable(TemplateView):
     def get_context_data(self, user_content=user_content, *args, **kwargs):
         context = super(ViewUsersTable, self).get_context_data(*args, **kwargs)
 
+
         def _set_user(usr):
             return {
                     'role_id': usr[0],
