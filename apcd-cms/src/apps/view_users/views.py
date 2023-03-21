f1327d8a94e549b527fd1b322f7c73fb78df76eb
davidmtacc <dmontoya@tacc.utexas.edu>
2023-03-21 10:25:41 -0700

    committing changes to switch branch

--- apcd-cms/src/apps/view_users/views.py
+++ apcd-cms/src/apps/view_users/views.py
@@ -12,13 +12,56 @@ class ViewUsersTable(TemplateView):
     user_content = get_users()
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated or not is_apcd_admin(request.user):
-           return HttpResponseRedirect('/')
+        #if not request.user.is_authenticated or not is_apcd_admin(request.user):
+           #return HttpResponseRedirect('/')
         return super(ViewUsersTable, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, user_content=user_content, *args, **kwargs):
         context = super(ViewUsersTable, self).get_context_data(*args, **kwargs)
         actions = 'View'
+
+        user_content = [
+            (
+                6,
+                3,
+                'aw@heck.org',
+                'Arnold Mathers',
+                'UHC & Nop',
+                'no create',
+                'none update',
+                'no notes',
+                't',
+                'submitter'
+
+            ),
+            (
+                5,
+                2,
+                'aw@heck.org',
+                'Jon Heck',
+                't and (t)',
+                'no create',
+                'none update',
+                'no notes',
+                't',
+                'submitter'
+
+            ),
+             (
+                 5,
+                 4,
+                'aw@heck.org',
+                'Penny Lago',
+                'UHC',
+                'no create',
+                'none update',
+                'no notes',
+                't',
+                'submitter'
+            ),           
+            
+        ]
+
 
 
         def _set_user(usr):
@@ -33,18 +76,27 @@ class ViewUsersTable(TemplateView):
                     'notes': usr[7],
                     'active': usr[8],
                     'role_name': usr[9],
+                    'view_modal_content': _set_modal_content(usr),
     
                 }
 
         def _set_modal_content(usr):
             return {
-                'notes': usr[7],
-                
+                    'role_id': usr[0],
+                    'user_id': usr[1],
+                    'user_email': usr[2],
+                    'user_name': usr[3],
+                    'org_name': usr[4],
+                    'created_at': usr[5],
+                    'updated_at': usr[6],
+                    'notes': usr[7],
+                    'active': usr[8],
+                    'role_name': usr[9],                
             }
         
         
 
-        context['header'] = ['User ID', 'Email', 'Name', 'Organization', 'Role', 'Active', 'Created', 'Updated', 'Notes']
+        context['header'] = ['User ID', 'Name', 'Organization', 'Role', 'Active', 'Action']
         context['rows'] = []
         for user in user_content:
             user_view = [usr for usr in user_content if usr[1] == user[0]]
