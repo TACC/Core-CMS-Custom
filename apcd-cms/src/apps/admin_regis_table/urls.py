da86a29e12f873fe6b3726ae400e5dc762727325
edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
2023-03-22 16:30:15 -0500

    task/FP-2013: View-based table filtering (#133)
    * Context filtering util
    
    * Registrations table app updates: add filter url pattern, update context to include filter options, selected filter, and filtered table entries
    
    * Add filter check to url pattern for paginator
    
    * Remove testing config for paginator
    
    * Moved table_filter util function to util/utils.py

--- apcd-cms/src/apps/admin_regis_table/urls.py
+++ apcd-cms/src/apps/admin_regis_table/urls.py
@@ -4,4 +4,5 @@ from apps.admin_regis_table.views import RegistrationsTable
 app_name = 'administration'
 urlpatterns = [
     path('list-registration-requests/', RegistrationsTable.as_view(), name='admin_regis_table'),
+    path(r'list-registration-requests/?filter=(?P<filter>)/', RegistrationsTable.as_view(), name='admin_regis_table'),
 ]
