fd6549cf3cb899735b08e701c3b5370bdb355880
davidmtacc <dmontoya@tacc.utexas.edu>
2023-03-22 20:14:10 -0700

    removed loop. removed extra code unneeded. made boolean show active or inactive.

--- apcd-cms/src/apps/view_users/views.py
+++ apcd-cms/src/apps/view_users/views.py
@@ -13,64 +13,17 @@ class ViewUsersTable(TemplateView):
     user_content = get_users()
 
     def dispatch(self, request, *args, **kwargs):
-        #if not request.user.is_authenticated or not is_apcd_admin(request.user):
-           #return HttpResponseRedirect('/')
+        if not request.user.is_authenticated or not is_apcd_admin(request.user):
+           return HttpResponseRedirect('/')
         return super(ViewUsersTable, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, user_content=user_content, *args, **kwargs):
         context = super(ViewUsersTable, self).get_context_data(*args, **kwargs)
         actions = 'View'
 
-        user_content = [
-            (
-                6,
-                3,
-                'aw@heck.org',
-                'Arnold Mathers',
-                'UHC & Nop',
-                'no create',
-                'none update',
-                'no notes',
-                't',
-                11,
-                'submitter'
-
-            ),
-            (
-                5,
-                2,
-                'aw@heck.org',
-                'Jon Heck',
-                't and (t)',
-                'no create',
-                'none update',
-                'no notes',
-                't',
-                10,
-                'submitter'
-
-            ),
-             (
-                 5,
-                 4,
-                'aw@heck.org',
-                'Penny Lago',
-                'UHC',
-                'no create',
-                'none update',
-                'no notes',
-                't',
-                9,
-                'submitter'
-            ),           
-            
-        ]
-
-
-
         def _set_user(usr):
             return {
-                    'view_modal_content': _set_modal_content(usr),
+                   
                     'role_id': usr[0],
                     'user_id': usr[1],
                     'user_email': usr[2],
@@ -82,27 +35,10 @@ class ViewUsersTable(TemplateView):
                     'active': usr[8],
                     'user_number': usr[9],
                     'role_name': usr[10],
-    
                 }
 
-        def _set_modal_content(usr):
-            return {
-                    'role_id': usr[0],
-                    'user_id': usr[1],
-                    'user_email': usr[2],
-                    'user_name': usr[3],
-                    'org_name': usr[4],
-                    'created_at': usr[5],
-                    'updated_at': usr[6],
-                    'notes': usr[7],
-                    'active': usr[8],
-                    'user_number': usr[9],
-                    'role_name': usr[10],                
-            }
-        
-        
 
-        context['header'] = ['User ID', 'Name', 'Organization', 'Role', 'Active', 'Number', 'Action']
+        context['header'] = ['User ID', 'Name', 'Organization', 'Role', 'Active', 'User Number', 'See More']
         context['rows'] = []
         for user in user_content:
             user_view = [usr for usr in user_content if usr[1] == user[0]]
