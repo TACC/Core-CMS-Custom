5408ddb84cc87aa869feaac637ea829a00991546
edmondsgarrett <43251554+edmondsgarrett@users.noreply.github.com>
2023-04-18 15:24:56 -0500

    task/WP-19: VIew users filtering in view rather than in template (#145)
    * Moved filtering to view: edited filter script in template to pass selected filter to view via XML and load page with new context; filtered context data based on selected filter and loaded that to template; added new url pattern to accommodate filter url param
    
    * Added another check to filter util to check for filter substring in filtered category data
    
    * Update apcd-cms/src/apps/view_users/urls.py
    
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>
    
    * Update apcd-cms/src/apps/view_users/urls.py
    
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sophia-massie <96220951+sophia-massie@users.noreply.github.com>

--- apcd-cms/src/apps/utils/utils.py
+++ apcd-cms/src/apps/utils/utils.py
@@ -11,6 +11,6 @@ def title_case(value):
 
 
 def table_filter(filter, table_data, filtered_category):
-    filtered_data = [row for row in table_data if row[filtered_category] == filter]
+    filtered_data = [row for row in table_data if row[filtered_category] == filter or filter in row[filtered_category]]
 
     return filtered_data
