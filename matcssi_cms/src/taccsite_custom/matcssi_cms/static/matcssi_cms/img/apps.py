ee38da4d09da934266c2eeca06c3a33156774ba3
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-05-24 12:56:00 -0500

    fix(matcssi_cms): remove misplaced apps.py

--- matcssi_cms/src/taccsite_custom/matcssi_cms/static/matcssi_cms/img/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-from taccsite_cms.settings import BASE_DIR
-
-class MatcssiCmsConfig(AppConfig):
-    name = 'taccsite_custom.matcssi_cms'
-    path = os.path.join(BASE_DIR, '/code/taccsite_custom/matcssi_cms')
