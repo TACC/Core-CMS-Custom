73eea4788b859d29e2c17355efb46aa6680c6f74
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-05-24 14:56:32 -0500

    fix(demdata-cms): remove unnecessary example app

--- demdata-cms/src/apps/custom_example/views.py
+++
@@ -1,9 +0,0 @@
-from curses.ascii import HT
-from django.http import HttpResponse
-from django.conf import settings
-from django.template import loader
-
-
-def AddedView(request):
-    template = loader.get_template('custom_example/custom_example.html')
-    return HttpResponse(template.render({}, request))
