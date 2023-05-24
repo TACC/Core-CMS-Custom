8f8b4e0b65046915d74c1b27aa126df838d9d1f8
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-05-24 09:48:32 -0500

    feat(matcssi_cms): initial commit

---
+++ matcssi_cms/src/taccsite_cms/settings_custom.py
@@ -0,0 +1,33 @@
+# CUSTOM SETTINGS VALUES.
+# TACC WMA CMS SITE:
+# *.MISE.TACC.UTEXAS.EDU
+
+
+########################
+# TACC: LOGOS
+########################
+
+LOGO = [ 
+    "matcssi",
+    "matcssi_cms/img/org_logos/matcssi.png",
+    "",
+    "/",
+    "_self",
+    "MatCSSI Logo for CMS/Portal",
+    "anonymous",
+    "True"
+]
+
+FAVICON = {
+    "img_file_src": "matcssi_cms/img/favicons/favicon.ico"
+}
+
+########################
+# DJANGO (EMAIL)
+########################
+
+# Set on server, NOT here
+# https://confluence.tacc.utexas.edu/x/coR9E
+# EMAIL_BACKEND = "..."
+# EMAIL_HOST = "..."
+# DEFAULT_FROM_EMAIL = "..."
