5b1335b0ae432c960d6faefc6a2b812be7928ab7
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-05-24 14:53:47 -0500

    fix(matcssi_cms): add missing updated content for secrets.default.py

--- matcssi_cms/src/taccsite_cms/secrets.default.py
+++ matcssi_cms/src/taccsite_cms/secrets.default.py
@@ -5,4 +5,15 @@
 # DATABASE SETTINGS
 ########################
 
-DATABASES['default']['HOST'] = 'matcssi_cms_postgres'
+from taccsite_cms.settings import DATABASES
+
+DATABASES = {
+    'default': {
+        'ENGINE': DATABASES['default']['ENGINE'],
+        'PORT': DATABASES['default']['PORT'],
+        'NAME': DATABASES['default']['NAME'],
+        'USER': DATABASES['default']['USER'],
+        'PASSWORD': DATABASES['default']['PASSWORD'],
+        'HOST': 'matcssi_cms_postgres'
+    }
+}
