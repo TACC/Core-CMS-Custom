40a467ee1dc37adc586c7c686809e8777429ec6e
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-06-22 16:40:31 -0500

    fix(demdata): once and for all

--- demdata_cms/src/taccsite_custom/demdata-cms/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'demdata_cms.apps.DemDataCMSConfig'
--- demdata_cms/src/taccsite_custom/demdata-cms/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class DemDataCMSConfig(AppConfig):
-    name = 'demdata_cms'  # or any other valid Python identifier
