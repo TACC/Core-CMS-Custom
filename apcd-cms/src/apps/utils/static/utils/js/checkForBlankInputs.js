e793fe90056852971b309f2c05339c73dd1f690f
Sophia Massie <smmassie92@gmail.com>
2023-08-22 17:25:40 -0500

    Allows special characters that users may use but still blacklists some script patterns in checkForBlankInputs

--- apcd-cms/src/apps/utils/static/utils/js/checkForBlankInputs.js
+++ apcd-cms/src/apps/utils/static/utils/js/checkForBlankInputs.js
@@ -1,7 +1,12 @@
 function noEmptyInputs(inputArr) {
   inputArr.forEach(i => i.addEventListener('change', () => {
-      if (i.value.replace(/ /g, '') === '') { 
-        i.value = ''; // if input is all spaces, want to clear it out to leverage <input>'s required prop
-      }
+    const cleanedValue = i.value.replace(/[<>';"/]|<script>|<\/script>|'|"|javascript|JavaScript|\.exe/gi, '');
+    console.log(cleanedValue);
+
+    if (cleanedValue.replace(/ /g, '') === '') { 
+      i.value = ''; // if input is all spaces, clear it out to leverage <input>'s required prop
+    } else {
+      i.value = cleanedValue; // Set the cleaned value back to the input
+    }
   }))
 }
