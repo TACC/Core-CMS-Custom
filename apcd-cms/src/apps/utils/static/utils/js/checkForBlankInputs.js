e481a52b9d0cfed6d2549635edb55d39d27b915a
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-08-23 17:13:40 -0500

    hotfix/WP-232-234-APCD-frontend-form-sanitation (#195)
    * Allows special characters that users may use but still
    blacklists some script patterns in checkForBlankInputs
    
    * Updated applicable input types that use validating checkForBlankInputs
    script
    
    * Added .sh and made sure all input fields are checked by script

--- apcd-cms/src/apps/utils/static/utils/js/checkForBlankInputs.js
+++ apcd-cms/src/apps/utils/static/utils/js/checkForBlankInputs.js
@@ -1,7 +1,11 @@
 function noEmptyInputs(inputArr) {
   inputArr.forEach(i => i.addEventListener('change', () => {
-      if (i.value.replace(/ /g, '') === '') { 
-        i.value = ''; // if input is all spaces, want to clear it out to leverage <input>'s required prop
-      }
+    const cleanedValue = i.value.replace(/[^a-zA-Z0-9\s-@!?,%$#&*().]|<script>|<\/script>|'|"|}|{|javascript|JavaScript|\.exe|\.sh/gi, '') // Allow specified characters
+
+    if (cleanedValue.replace(/ /g, '') === '') { 
+      i.value = ''; // if input is all spaces, clear it out to leverage <input>'s required prop
+    } else {
+      i.value = cleanedValue; // Set the cleaned value back to the input
+    }
   }))
 }
