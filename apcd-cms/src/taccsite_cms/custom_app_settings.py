198a66cc7adee22da0831cc096f5c879b7e119cf
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2023-08-30 15:02:55 -0500

    Hotfix/apcd prod remove apps (#216)
    * Remove admin, exception, extension, and submission table from prod
    
    * Remove unneeded paginator app
    
    * Remove paginator

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,5 +1,5 @@
 CUSTOM_APPS = ['apps.apcd_login', 'apps.registrations', 'apps.utils']
 CUSTOM_MIDDLEWARE = []
-STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/components/paginator', 'apps/utils')
+STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/utils')
 # Removed apps not needed for prod 8.30.2023 
 # Check file history to add back admin, exception, extension, and submission apps
