5c22e9e494aa1544a01f06a3c4f4a9431a244b12
Sophia Massie <smmassie92@gmail.com>
2023-08-30 11:52:00 -0500

    Removed admin, exception, extension, and user submission apps from prod branch

--- apcd-cms/src/taccsite_cms/custom_app_settings.py
+++ apcd-cms/src/taccsite_cms/custom_app_settings.py
@@ -1,4 +1,5 @@
-CUSTOM_APPS = ['apps.admin_regis_table', 'apps.apcd_login', 'apps.registrations', 'apps.submissions', 'apps.exception', 'apps.admin_submissions', 'apps.admin_extension', 'apps.admin_exception', 'apps.extension', 'apps.view_users', 'apps.components.paginator', 'apps.utils']
+CUSTOM_APPS = ['apps.apcd_login', 'apps.registrations', 'apps.components.paginator', 'apps.utils']
 CUSTOM_MIDDLEWARE = []
-STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/admin_regis_table', 'apps/submissions', 'apps/exception', 'apps/extension', 'apps/view_users', 'apps/components/paginator', 'apps/utils')
-
+STATICFILES_DIRS = ('taccsite_custom/apcd-cms', 'apps/components/paginator', 'apps/utils')
+# Removed apps not needed for prod 8.30.2023 
+# Check file history to add back admin, exception, extension, and submission apps
