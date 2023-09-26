8d38999ea128ccaaa0c2fe639012f3fda1abc433
Mariah J <mariah.james@tmc.uth.edu>
2023-09-26 19:30:42 +0000

    Add custom paginator to interact with APCD API

---
+++ apcd-cms/src/apps/components/paginator/api_pagination.py
@@ -0,0 +1,101 @@
+from django.utils.functional import cached_property
+from django.core.paginator import Paginator, EmptyPage
+import logging
+import collections.abc
+
+logger = logging.getLogger(__name__)
+
+
+class APIPagination(Paginator):
+    def __init__(self, table_rows, entries_per_page, total_count):
+        """
+
+        :param table_rows: paginator object_list
+        :param entries_per_page: entries per page (paginator - per_page)
+        :param total_count: Total count of record found in db -recieved from APCD API
+        """
+        self.total_count = total_count
+        super(APIPagination, self).__init__(table_rows, entries_per_page)
+
+    @cached_property
+    def count(self):
+        return self.total_count
+
+    def page(self, number):
+        """Return a Page object for the given 1-based page number."""
+        number = self.validate_number(number)
+        bottom = (number - 1) * self.per_page
+        top = bottom + self.per_page
+        if top + self.orphans >= self.count:
+            top = self.count
+        # return self._get_page(self.object_list[bottom:top], number, self)
+        return self._get_page(self.object_list, number, self)
+
+    def _get_page(self, *args, **kwargs):
+        """
+        Return an instance of a single page.
+
+        This hook can be used by subclasses to use an alternative to the
+        standard :cls:`Page` object.
+        """
+        return Page(*args, **kwargs)
+
+
+class Page(collections.abc.Sequence):
+    def __init__(self, object_list, number, paginator):
+        self.object_list = object_list
+        self.number = number
+        self.paginator = paginator
+
+    def __repr__(self):
+        return "<Page %s of %s>" % (self.number, self.paginator.num_pages)
+
+    def __len__(self):
+        return len(self.object_list)
+
+    def __getitem__(self, index):
+        if not isinstance(index, (int, slice)):
+            raise TypeError(
+                "Page indices must be integers or slices, not %s."
+                % type(index).__name__
+            )
+        # The object_list is converted to a list so that if it was a QuerySet
+        # it won't be a database hit per __getitem__.
+        if not isinstance(self.object_list, list):
+            self.object_list = list(self.object_list)
+        return self.object_list[index]
+
+    def has_next(self):
+        return self.number < self.paginator.num_pages
+
+    def has_previous(self):
+        return self.number > 1
+
+    def has_other_pages(self):
+        return self.has_previous() or self.has_next()
+
+    def next_page_number(self):
+        return self.paginator.validate_number(self.number + 1)
+
+    def previous_page_number(self):
+        return self.paginator.validate_number(self.number - 1)
+
+    def start_index(self):
+        """
+        Return the 1-based index of the first object on this page,
+        relative to total objects in the paginator.
+        """
+        # Special case, return zero if no items.
+        if self.paginator.count == 0:
+            return 0
+        return (self.paginator.per_page * (self.number - 1)) + 1
+
+    def end_index(self):
+        """
+        Return the 1-based index of the last object on this page,
+        relative to total objects found (hits).
+        """
+        # Special case for the last page because there can be orphans.
+        if self.number == self.paginator.num_pages:
+            return self.paginator.count
+        return self.number * self.paginator.per_page
