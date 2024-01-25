72ab119f18c9bd970f39de6fa4f421678eb5e926
Garrett Edmonds <gedmonds@gedmonds-mbp24.local>
2024-01-25 17:55:12 -0600

    Remove some logging used for testing

--- apcd-cms/src/apps/utils/apcd_groups.py
+++ apcd-cms/src/apps/utils/apcd_groups.py
@@ -1,7 +1,3 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
 def has_apcd_group(user):
     for group in user.groups.all():
         if group.name in ['APCD_ADMIN', 'SUBMITTER_ADMIN', 'SUBMITTER_USER']:
@@ -12,6 +8,5 @@ def is_apcd_admin(user):
     return user.groups.filter(name='APCD_ADMIN').exists()
 
 def has_groups(user, groups):
-    logger.error([user_group for user_group in user.groups.all()])
     return len([user_group for user_group in user.groups.all() if user_group.name in groups]) > 0  # if user has permission group(s) in requested list, this 
                                                                                               # intersection's length should be non-zero
