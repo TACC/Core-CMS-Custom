b72403a0a92c548e62a494e4ffaa3e4c91aca167
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-02-12 18:14:18 -0600

    chore(demdata): remove branding (css hides it)
    The original, fast solution was to hide it with CSS.
    
    Just setting BRANDING to False should have the same effect.

--- demdata_cms/src/taccsite_cms/settings_custom.py
+++ demdata_cms/src/taccsite_cms/settings_custom.py
@@ -9,6 +9,7 @@
 CMS_TEMPLATES = (
     ('standard.html', 'Standard'),
     ('fullwidth.html', 'Full Width'),
+
     ('demdata_cms/templates/standard.html', 'Standard (with CSS from TUP)'),
     ('demdata_cms/templates/fullwidth.html', 'Full Width (with CSS from TUP)'),
     ('demdata-cms/templates/standard.html', 'DEPRECATED Standard (with CSS from TUP)'),
@@ -19,45 +20,7 @@ CMS_TEMPLATES = (
 # TACC: BRANDING
 ########################
 
-# LOOK INTO THIS SOLUTION.
-# from taccsite_cms.settings import TACC_BRANDING, UTEXAS_BRANDING, NSF_BRANDING
-
-TACC_BRANDING = [
-    "tacc",
-    "site_cms/img/org_logos/tacc-white.png",
-    "branding-tacc",
-    "https://www.tacc.utexas.edu/",
-    "_blank",
-    "TACC Logo",
-    "anonymous",
-    "True"
-]
-
-UTEXAS_BRANDING = [
-    "utexas",
-    "site_cms/img/org_logos/utaustin-white.png",
-    "branding-utaustin",
-    "https://www.utexas.edu/",
-    "_blank",
-    "University of Texas at Austin Logo",
-    "anonymous",
-    "True"
-]
-
-# NSF_BRANDING = [
-#     "nsf",
-#     "site_cms/img/org_logos/nsf-white.png",
-#     "branding-nsf",
-#     "https://www.nsf.gov/",
-#     "_blank",
-#     "NSF Logo",
-#     "anonymous",
-#     "True"
-# ]
-
-# BRANDING = [ TACC_BRANDING, UTEXAS_BRANDING, NSF_BRANDING ]
-BRANDING = [ TACC_BRANDING, UTEXAS_BRANDING ] # this matches prod 2022
-# BRANDING = [] # prod 2022 hides bar via snippet CSS but should also do this
+BRANDING = False
 
 ########################
 # TACC: LOGO & FAVICON
