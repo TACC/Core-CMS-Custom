91a274f7877ea255312b42b1f0281acb0b3bd0e1
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-02-12 18:07:08 -0600

    chore(ecep): remove unused templates

--- ecep_cms/src/taccsite_cms/settings_custom.py
+++ ecep_cms/src/taccsite_cms/settings_custom.py
@@ -9,12 +9,6 @@
 CMS_TEMPLATES = (
     ('standard.html', 'Standard'),
     ('fullwidth.html', 'Full Width'),
-
-    ('guide.html', 'Guide'),
-    ('guides/getting_started.html', 'Guide: Getting Started'),
-    ('guides/data_transfer.html', 'Guide: Data Transfer'),
-    ('guides/data_transfer.globus.html', 'Guide: Globus Data Transfer'),
-    ('guides/portal_technology.html', 'Guide: Portal Technology Stack')
 )
 
 ########################
