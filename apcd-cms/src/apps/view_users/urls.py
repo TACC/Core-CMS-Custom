706a0b53bc172d4bcf5333d035d191d55480a12e
Van Go <35277477+van-go@users.noreply.github.com>
2024-06-10 16:14:46 -0500

    chore: Update Docker Compose dev configuration for React

--- apcd-cms/src/apps/view_users/urls.py
+++ apcd-cms/src/apps/view_users/urls.py
@@ -4,7 +4,7 @@ from apps.view_users.views import ViewUsersTable
 
 app_name = 'administration'
 urlpatterns = [
-    path('view-users/', TemplateView.as_view(template_name='list_registrations.html'), name='view_users'),
-    path('view-users/api', ViewUsersTable.as_view(), name='view_users_api'),
-    path('view-users/api/<str:filter>/', ViewUsersTable.as_view(), name='view_users_api')
+    path('view-users/', TemplateView.as_view(template_name='view_users.html'), name='view_users'),
+    path(r'view-users/api', ViewUsersTable.as_view(), name='view_users_api'),
+    path(r'view-users/api/<str:filter>/', ViewUsersTable.as_view(), name='view_users_api')
 ]
