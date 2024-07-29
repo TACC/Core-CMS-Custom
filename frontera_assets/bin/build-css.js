5468a6ab0137656019a48707a2bf24a1331d369b
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-07-29 12:52:28 -0500

    refactor(frontera): WP-197 reduce css build scope

--- frontera_assets/bin/build-css.js
+++ frontera_assets/bin/build-css.js
@@ -5,12 +5,20 @@ const buildStylesheets = require('@tacc/core-styles').buildStylesheets;
 const ROOT = __dirname + '/..';
 const BUILD_ID = process.env.BUILD_ID || process.env.npm_package_version;
 
+const options = {
+  verbose: true,
+  fileExt: '.css',
+  buildId: BUILD_ID,
+  customConfigs: [`${ROOT}/.postcssrc.yml`]
+};
+
 buildStylesheets(
-  /* input */ `${ROOT}/css/src/**/*.css`,
-  /* output */ `${ROOT}/css/build`,
-  /* options */ {
-    verbose: true,
-    buildId: BUILD_ID,
-    customConfigs: [`${ROOT}/.postcssrc.yml`]
-  }
+  /* input */ `${ROOT}/css/src/_imports/trumps/s-home.postcss`,
+  /* output */ `${ROOT}/css/src/_imports/trumps/`,
+  /* options */ options
+);
+buildStylesheets(
+  /* input */ `${ROOT}/css/src/_migrations/v1_v2/frontera.postcss`,
+  /* output */ `${ROOT}/css/src/_migrations/v1_v2/`,
+  /* options */ options
 );
