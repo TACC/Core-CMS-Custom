599926c523d5584608043c76c0819eadff2c3b07
Carrie Arnold <cmarnold@tacc.utexas.edu>
2024-08-16 13:39:12 -0500

    List Admin Extensions React Conversion (#289)
    * first run at the react conversion
    
    * Update list_admin_extension.html from merge feat branch
    
    * Updated incorrect import
    
    ---------
    
    Co-authored-by: Chandra Y <cyemparala@tacc.utexas.edu>

--- apcd-cms/src/client/src/main.tsx
+++ apcd-cms/src/client/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AdminRegistrations } from './components/Admin/Registrations';
 import { ViewUsers } from './components/Admin/ViewUsers';
 import { AdminSubmissions } from './components/Admin/Submissions';
+import { AdminExtensions } from './components/Admin/Extensions';
 import { AdminExceptions } from './components/Admin/Exceptions';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
@@ -29,6 +30,7 @@ const componentMap: { [key: string]: React.ComponentType<any> } = {
   'list-registrations-root': AdminRegistrations,
   'view-users-root': ViewUsers,
   'list-admin-submissions': AdminSubmissions,
+  'admin-extensions-root': AdminExtensions,
   'admin-exceptions-root': AdminExceptions,
   // Add new components with html id in the list above.
 };
