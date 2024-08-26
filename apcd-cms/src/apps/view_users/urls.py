530fa5088bad18323c048410e2055d917b824f98
Van Go <35277477+van-go@users.noreply.github.com>
2024-08-26 11:21:58 -0500

    temp commit

--- apcd-cms/src/apps/view_users/urls.py
+++ apcd-cms/src/apps/view_users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from apps.view_users.views import ViewUsersTable
+from apps.view_users.views import ViewUsersTable, update_user_view
 
 app_name = 'administration'
 urlpatterns = [
     path('view-users/', TemplateView.as_view(template_name='view_users.html'), name='view_users'),
     path('view-users/api/', ViewUsersTable.as_view(), name='view_users_api'),
     path('view-users/api/options', ViewUsersTable.as_view(), name='view_users_api_options'),
-    path('view-users/api/modal/<str:modal_type>/', ViewUsersTable.as_view(), name='view_users_modal')
+    path('view-users/api/modal/<str:modal_type>/', ViewUsersTable.as_view(), name='view_users_modal'),
+    path('users/<int:user_number>/', update_user_view, name='update_user'), 
 ]
