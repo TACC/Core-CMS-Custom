d5145c119aa564ebfc790c09e7891d1c3cd41e59
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-09-30 18:01:09 -0500

    enhance(ecep): from _cms to _assets

--- ecep_cms/src/taccsite_cms/urls_custom.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, re_path
-
-custom_urls = [
-    # Support `taggit_autosuggest` (from `djangocms-blog`)
-    re_path(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
-]
