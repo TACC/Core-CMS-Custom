40edca19d16af93244df33b7411f427aee00bc6f
sophia-massie <smassie@tacc.utexas.edu>
2024-10-24 20:04:10 -0500

    - Unstaged changes left behind from last commit

--- apcd-cms/src/apps/exception/views.py
+++ apcd-cms/src/apps/exception/views.py
@@ -11,32 +11,19 @@ logger = logging.getLogger(__name__)
 
 class ExceptionFormView(TemplateView):
     def post(self, request):
-        logger.debug("Am I here" * 10)
         if (request.user.is_authenticated) and has_apcd_group(request.user):
-            form = request.POST.copy()
-            logger.debug(print(form))
+            form = json.loads(request.body)
+            exceptions = form['exceptions']
             errors = []
             submitters = apcd_database.get_submitter_info(request.user.username)
-            # To create counter of exception requests and corresponding fields
-            max_iterations = 1
-            for i in range(2, 6):
-                if form.get('field-threshold-exception_{}'.format(i)):
-                    max_iterations += 1
-                else:
-                    break
-
-            for iteration in range(max_iterations):
-                submitter = next(submitter for submitter in submitters if int(submitter[0]) == int(form['business-name_{}'.format(iteration + 1)]))
-                if _err_msg(submitter):
-                    errors.append(_err_msg(submitter))
-                except_response = apcd_database.create_threshold_exception(form, iteration + 1, submitter)
-                if _err_msg(except_response):
-                    errors.append(_err_msg(except_response))
-
+            for exception in exceptions:
+                submitter = next(submitter for submitter in submitters if int(submitter[0] == int(exception['businessName'])))
+                exception_response = apcd_database.create_threshold_exception(form, exception, submitter)
+            if exception_response:
+                errors.append(exception_response)
             if errors:
                 return JsonResponse({'status': 'error', 'errors': errors}, status=400)
-            else:
-                return JsonResponse({'status': 'success'}, status=200)
+            return JsonResponse({'status': 'success'}, status=200)
 
         else:
             return HttpResponseRedirect('/')
@@ -46,9 +33,3 @@ class ExceptionFormView(TemplateView):
             return HttpResponseRedirect('/')
         return super(ExceptionFormView, self).dispatch(request, *args, **kwargs)
 
-def _err_msg(resp):
-    if hasattr(resp, "pgerror"):
-        return resp.pgerror
-    if isinstance(resp, Exception):
-        return str(resp)
-    return None
