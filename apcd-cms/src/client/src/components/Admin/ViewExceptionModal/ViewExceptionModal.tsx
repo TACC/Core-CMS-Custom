cf67a9a0e84dff3f76afadf5033098cd8ed83dc8
Juan Martinez <jmartinez@tacc.utexas.edu>
2024-11-01 10:23:53 -0500

    Modal windows review changes.

--- apcd-cms/src/client/src/components/Admin/ViewExceptionModal/ViewExceptionModal.tsx
+++ apcd-cms/src/client/src/components/Admin/ViewExceptionModal/ViewExceptionModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Row, Col } from 'reactstrap';
 import { ExceptionModalContent, ExceptionRow } from 'hooks/admin';
 import styles from './ViewExceptionModal.module.css';
 
@@ -31,7 +31,7 @@ export const ViewExceptionModal: React.FC<{
   return (
     <Modal title="View Exception" isOpen={isOpen} toggle={onClose} size="lg">
       <div className="modal-header">
-        <h4 className="modal-title text-capitalize">View Exception</h4>
+        <h4 className="modal-title text-capitalize">Exception Details</h4>
         <button type="button" className="close" onClick={onClose}>
           <span aria-hidden="true">&#xe912;</span>
         </button>
@@ -39,120 +39,121 @@ export const ViewExceptionModal: React.FC<{
       <ModalBody className="modal-body">
         <div>
           <h4>Details</h4>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Created</dt>
-              <dd className={styles.verticalDataValue}>
-                {new Date(created_at).toLocaleString()}
-              </dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Entity Organization</dt>
-              <dd className={styles.verticalDataValue}>{entity_name}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Requestor</dt>
-              <dd className={styles.verticalDataValue}>{requestor_name}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Requestor Email</dt>
-              <dd className={styles.verticalDataValue}>{requestor_email}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Exception Type</dt>
-              <dd className={styles.verticalDataValue}>{request_type}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Status</dt>
-              <dd className={styles.verticalDataValue}>{status}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Outcome</dt>
-              <dd className={styles.verticalDataValue}>{outcome}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">File Type</dt>
-              <dd className={styles.verticalDataValue}>{data_file_name}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Field Number</dt>
-              <dd className={styles.verticalDataValue}>{field_number}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Required Threshold</dt>
-              <dd className={styles.verticalDataValue}>{required_threshold}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Requested Threshold</dt>
-              <dd className={styles.verticalDataValue}>
-                {requested_threshold}
-              </dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Approved Threshold</dt>
-              <dd className={styles.verticalDataValue}>{approved_threshold}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Requested Expiration Date</dt>
-              <dd className={styles.verticalDataValue}>
-                {requested_expiration_date}
-              </dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Approved Expiration Date</dt>
-              <dd className={styles.verticalDataValue}>
-                {approved_expiration_date}
-              </dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Explanation Justification</dt>
-              <dd className={styles.verticalDataValue}>
-                {explanation_justification}
-              </dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Exception Notes</dt>
-              <dd className={styles.verticalDataValue}>{notes}</dd>
-            </dl>
-          </dd>
-          <dd>
-            <dl className={styles.verticalDataList}>
-              <dt className="c-data-list__key">Last Updated</dt>
-              <dd className={styles.verticalDataValue}>
-                {new Date(updated_at).toLocaleString()}
-              </dd>
-            </dl>
-          </dd>
+          <div className="modal-section">
+            <Row>
+              <Col md={{size: 4, offset: 1}}>Created</Col>
+              <Col md={7}>
+                  {new Date(created_at).toLocaleString()}
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{size: 4, offset: 1}}>Entity Organization</Col>
+              <Col md={7}>
+              {entity_name}
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{size: 4, offset: 1}}>Requestor</Col>
+              <Col md={7}>
+              {requestor_name}
+              </Col>
+            </Row>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Requestor Email</dt>
+                <dd className={styles.verticalDataValue}>{requestor_email}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Exception Type</dt>
+                <dd className={styles.verticalDataValue}>{request_type}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Status</dt>
+                <dd className={styles.verticalDataValue}>{status}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Outcome</dt>
+                <dd className={styles.verticalDataValue}>{outcome || 'None'}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">File Type</dt>
+                <dd className={styles.verticalDataValue}>{data_file_name}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Field Number</dt>
+                <dd className={styles.verticalDataValue}>{field_number}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Required Threshold</dt>
+                <dd className={styles.verticalDataValue}>{required_threshold || 'None'}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Requested Threshold</dt>
+                <dd className={styles.verticalDataValue}>
+                  {requested_threshold}
+                </dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Approved Threshold</dt>
+                <dd className={styles.verticalDataValue}>{approved_threshold || 'Nome'}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Requested Expiration Date</dt>
+                <dd className={styles.verticalDataValue}>
+                  {requested_expiration_date}
+                </dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Approved Expiration Date</dt>
+                <dd className={styles.verticalDataValue}>
+                  {approved_expiration_date || 'None'}
+                </dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Explanation Justification</dt>
+                <dd className={styles.verticalDataValue}>
+                  {explanation_justification}
+                </dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Exception Notes</dt>
+                <dd className={styles.verticalDataValue}>{notes || 'None'}</dd>
+              </dl>
+            </dd>
+            <dd>
+              <dl className={styles.verticalDataList}>
+                <dt className="c-data-list__key">Last Updated</dt>
+                <dd className={styles.verticalDataValue}>
+                  {new Date(updated_at).toLocaleString()}
+                </dd>
+              </dl>
+            </dd>
+            <hr />
+          </div>
         </div>
       </ModalBody>
     </Modal>
