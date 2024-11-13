1d12a431dab86a86a6eac336e79537141c0544ef
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2024-11-13 11:35:35 -0600

    Task/wp 751 submission listing bugs (#357)
    * - Updated view and url to get filters and filtered
    submission content
    - Moved styles from app to client
    - Added sort options to filter hooks
    - Created client side title case util so it won't
    break pages on response if response does not exist
    
    * - Staged the wrong changes. WIP from Nov 12
    
    * - Adds filter styles to filter-contents class rather
    that hard coding
    
    * - Added some debug statements to find where
    the modal data goes missing
    
    * - Final configuring of both submitter submission page
    and admin submission page
    
    * - Catches undefined for string utils
    - Removes repeated header
    
    * - Linting
    
    ---------
    
    Co-authored-by: Chandra Y <cyemparala@tacc.utexas.edu>

--- apcd-cms/src/client/src/components/Submissions/ViewFileSubmissions/ViewSubmissions.tsx
+++ apcd-cms/src/client/src/components/Submissions/ViewFileSubmissions/ViewSubmissions.tsx
@@ -1,34 +1,79 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { useListSubmissions, FileSubmissionRow } from 'hooks/submissions';
+import {
+  useListSubmissions,
+  FileSubmissionRow,
+  useSubmissionFilters,
+  FileSubmissionLogsModalContent,
+} from 'hooks/submissions';
+import LoadingSpinner from 'core-components/LoadingSpinner';
+import Button from 'core-components/Button';
+import SectionMessage from 'core-components/SectionMessage';
+import Paginator from 'core-components/Paginator';
+import styles from './ViewSubmissions.module.css';
+import { Link } from 'react-router-dom';
 import { FileSubmissionLogsModal } from './ViewSubmissionsModal';
 import { formatDate } from 'utils/dateUtil';
+import { titleCase } from 'utils/stringUtil';
 
 export const ViewFileSubmissions: React.FC = () => {
-  const [status, setStatus] = useState<string>('');
-  const [sort, setSort] = useState<string>('');
-  const [page, setPage] = useState<number>(1);
+  const header = [
+    'Received',
+    'File Name',
+    'Outcome',
+    'Status',
+    'Last Updated',
+    'Actions',
+  ];
+  const {
+    data: filterData,
+    isLoading: isFilterLoading,
+    isError: isFilterError,
+  } = useSubmissionFilters();
 
-  const { data, isLoading, isError, refetch } = useListSubmissions(
-    status,
-    sort,
-    page
-  );
+  const [status, setStatus] = useState<string>('All');
+  const [sort, setSort] = useState<string>('Newest Received');
+  const [page, setPage] = useState<number>(1);
+  const [viewModalOpen, setViewModalOpen] = useState(false);
+  const [selectedSubmission, setSelectedSubmission] =
+    useState<FileSubmissionRow | null>(null);
+  const [selectedSubmissionLog, setSelectedSubmissionLog] = useState<
+    FileSubmissionLogsModalContent[]
+  >([]);
+
+  const closeModal = () => {
+    setViewModalOpen(false);
+    setSelectedSubmission(null);
+  };
+
+  const {
+    data: submissionData,
+    isLoading: isSubmissionLoading,
+    isError: isSubmissionError,
+    refetch,
+  } = useListSubmissions(status, sort, page);
 
   useEffect(() => {
     refetch();
   }, [status, sort, page]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (isSubmissionLoading) {
+    return <LoadingSpinner />;
   }
 
-  if (isError) {
-    return <div>Error loading data</div>;
+  if (isSubmissionError) {
+    return (
+      <SectionMessage type="error">
+        There was an error loading the page.{' '}
+        <Link to="/workbench/dashboard/tickets/create" className="wb-link">
+          Please submit a ticket.
+        </Link>
+      </SectionMessage>
+    );
   }
 
   const clearSelections = () => {
-    setStatus('');
-    setSort('');
+    setStatus('All');
+    setSort('All');
     setPage(1);
   };
 
@@ -37,74 +82,89 @@ export const ViewFileSubmissions: React.FC = () => {
       <div className="filter-container">
         <div className="filter-content">
           {/* Filter */}
-          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-            <span>
-              <b>Filter by Status: </b>
-            </span>
-            <select
-              id="statusFilter"
-              className="status-filter"
-              defaultValue={data?.selected_status} // Use defaultValue to set the initial selected value
-              onChange={(e) => setStatus(e.target.value)}
-            >
-              {data?.filter_options.map((status, index) => (
-                <option className="dropdown-text" key={index} value={status}>
-                  {status}
-                </option>
-              ))}
-            </select>
-            <span>
-              <b>Sort by: </b>
-            </span>
-            <select
-              id="dateSort"
-              className="status-filter"
-              defaultValue=""
-              onChange={(e) => setSort(e.target.value)}
-            >
-              {Object.entries(data?.sort_options || {}).map(
-                (option: any, index: number) => (
-                  <option
-                    className="dropdown-text"
-                    key={index}
-                    value={option[0]}
-                  >
-                    {option[1]}
-                  </option>
-                )
-              )}
-            </select>
-            {data?.selected_status || data?.selected_sort ? (
-              <button onClick={clearSelections}>Clear Options</button>
-            ) : null}
-          </div>
+          <span>
+            <b>Filter by Status: </b>
+          </span>
+          <select
+            id="statusFilter"
+            className="status-filter"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          >
+            {filterData?.status_options.map((option: any, index: number) => (
+              <option className="dropdown-text" key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <span>
+            <b>Sort by: </b>
+          </span>
+          <select
+            id="dateSort"
+            className="status-filter"
+            value={sort === '' ? filterData?.sort_options[0] : sort}
+            onChange={(e) => setSort(e.target.value)}
+          >
+            {filterData?.sort_options.map((option: any, index: number) => (
+              <option className="dropdown-text" key={index} value={option.name}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+          {status !== 'All' || sort !== 'Newest Received' ? (
+            <button onClick={clearSelections}>Clear Options</button>
+          ) : null}
         </div>
       </div>
-      <table id="submissionTable" className="submission-table">
+      <table id="submissionTable" className={styles.submissionTable}>
         <thead>
           <tr>
-            {data?.header.map((columnName: string, index: number) => (
+            {header.map((columnName: string, index: number) => (
               <th key={index}>{columnName}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data?.page.map((row: FileSubmissionRow, rowIndex: number) => (
-            <tr key={rowIndex}>
-              <td>{formatDate(row.received_timestamp)}</td>
-              <td>{row.file_name}</td>
-              <td>{row.outcome}</td>
-              <td>{row.status}</td>
-              <td>{row.updated_at}</td>
-              <td>
-                <FileSubmissionLogsModal
-                  submission_logs={row.view_modal_content}
-                />
-              </td>
-            </tr>
-          ))}
+          {submissionData?.page.map(
+            (row: FileSubmissionRow, rowIndex: number) => (
+              <tr key={rowIndex}>
+                <td>{formatDate(row.received_timestamp)}</td>
+                <td>{titleCase(row.file_name)}</td>
+                <td>{titleCase(row.outcome)}</td>
+                <td>{titleCase(row.status)}</td>
+                <td>{formatDate(row.updated_at)}</td>
+                <td>
+                  <Button
+                    type="link"
+                    onClick={() => {
+                      setSelectedSubmission(row);
+                      setSelectedSubmissionLog(row?.view_modal_content);
+                      setViewModalOpen(true);
+                    }}
+                  >
+                    View Logs
+                  </Button>
+                </td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
+      <div className={styles.paginatorContainer}>
+        <Paginator
+          pages={submissionData?.total_pages ?? 0}
+          current={submissionData?.page_num ?? 0}
+          callback={setPage}
+        />
+      </div>
+      {selectedSubmission && viewModalOpen && (
+        <FileSubmissionLogsModal
+          isOpen={viewModalOpen}
+          parentToggle={closeModal}
+          submission_logs={selectedSubmissionLog}
+        />
+      )}
     </div>
   );
 };
