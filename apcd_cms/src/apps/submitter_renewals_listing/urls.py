85903f18f2ad260c978dc791f09e446af3692af5
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-11-27 09:18:00 -0600

    Manual merge for apcd - folder rename (#376)
    Co-authored-by: Chandra Y <cyemparala@tacc.utexas.edu>

--- apcd_cms/src/apps/submitter_renewals_listing/urls.py
+++ apcd_cms/src/apps/submitter_renewals_listing/urls.py
@@ -5,9 +5,5 @@ from apps.submitter_renewals_listing.views import SubmittersTable
 app_name = 'register'
 urlpatterns = [
     path('list-registration-requests/', TemplateView.as_view(template_name='list_submitter_registrations.html'), name='submitter_regis_table'),
-    path(r'list-registration-requests/api/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
-    path(r'list-registration-requests/api/?status=(?P<status>)/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
-    path(r'list-registration-requests/api/?org=(?P<org>)/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
-    path(r'list-registration-requests/api/?status=(?P<status>)&org=(?P<org>)/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
-    path(r'list-registration-requests/api/?reg_id=(?P<reg_id>)/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
+    path('list-registration-requests/api/', SubmittersTable.as_view(), name='submitter_regis_table_api'),
 ]
