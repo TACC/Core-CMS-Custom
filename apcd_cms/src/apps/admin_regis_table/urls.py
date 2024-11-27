1890a53fd204deec767bda14a98be5d3505655fa
Chandra Y <cyemparala@tacc.utexas.edu>
2024-11-27 09:17:22 -0600

    Manual merge for apcd - folder rename

--- apcd_cms/src/apps/admin_regis_table/urls.py
+++ apcd_cms/src/apps/admin_regis_table/urls.py
@@ -5,11 +5,6 @@ from apps.admin_regis_table.views import RegistrationsTable
 app_name = 'admin_regis_table'
 urlpatterns = [
     path('list-registration-requests/', TemplateView.as_view(template_name='list_registrations.html'), name='admin_regis_table'),
-    path(r'list-registration-requests/api/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
-    path(r'list-registration-requests/api/?status=(?P<status>)/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
-    path(r'list-registration-requests/api/?org=(?P<org>)/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
-    path(r'list-registration-requests/api/?status=(?P<status>)&org=(?P<org>)/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
-    path(r'list-registration-requests/api/?reg_id=(?P<reg_id>)/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
-    path(r'request-to-submit/api/<int:reg_id>/', RegistrationsTable.as_view(), name='admin_regis_update_api'),
-
+    path('list-registration-requests/api/', RegistrationsTable.as_view(), name='admin_regis_table_api'),
+    path('request-to-submit/api/<int:reg_id>/', RegistrationsTable.as_view(), name='admin_regis_update_api'),
 ]
