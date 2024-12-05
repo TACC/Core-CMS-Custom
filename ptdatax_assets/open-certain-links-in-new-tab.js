5ced0850f852e06d614742efa4d8277fffe2dc47
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-12-04 18:50:52 -0600

    chore(ptdatax): directly load Core-CMS script

--- ptdatax_assets/open-certain-links-in-new-tab.js
+++ ptdatax_assets/open-certain-links-in-new-tab.js
@@ -1,5 +1,4 @@
-/* TODO: After deploy of Core-CMS v4.21.0, load script from `/static/site_cms/js/modules/setTargetForExternalLinks.js` */
-import findLinksAndSetTargets from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@v4.21.0/taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js';
+import findLinksAndSetTargets from '/static/site_cms/js/modules/setTargetForExternalLinks.js';
 
 const links = document.querySelectorAll(
     'a[href*="/core/internal-docs/"]'
