66b10bde220fd83eb290f5407eb89befb0ac217b
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-12-04 18:53:42 -0600

    feat(ptdatax): cmd-193 new tab links (#389)
    * feat(ptdatax): CMD-193 new tab links
    
    * fix(ptdatax): CMD-193 new tab links bug
    
    * fix(ptdatax): CMD-193 new tab links typo
    
    * fix(ptdatax): CMD-193 new tab links typo 2
    
    * deps(ptdatax): CMD-193 new tab links new version
    
    * chore(ptdatax): CMD-193 via Core-CMS v4.21.0
    
    * docs(ptdatax): Core-CMS v4.21.0 post-deploy task
    
    * chore(ptdatax): directly load Core-CMS script
    
    * chore(ptdatax): explicitely do not debug

---
+++ ptdatax_assets/open-certain-links-in-new-tab.js
@@ -0,0 +1,9 @@
+import findLinksAndSetTargets from '/static/site_cms/js/modules/setTargetForExternalLinks.js';
+
+const links = document.querySelectorAll(
+    'a[href*="/core/internal-docs/"]'
+);
+findLinksAndSetTargets( links, {
+    shouldDebug: false,
+    shouldFilter: false,
+});
