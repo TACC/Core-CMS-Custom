f9ac653ed999e20211533ddf8c89f0788242a607
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-12-04 18:00:00 -0600

    feat(ptdatax): CMD-193 new tab links

---
+++ ptdatax_assets/open-certain-links-in-new-tab.js
@@ -0,0 +1,9 @@
+/* TODO: After deploy of Core-CMS version that has https://github.com/TACC/Core-CMS/pull/902, change this snippet to load `/static/site_cms/js/modules/setTargetForExternalLinks.js` */
+import findLinksAndSetTargets from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@582f626e/taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js';
+
+const links = document.querySelectorAll(
+    'a[href*="/core/internal-docs/"]'
+);
+findLinksAndSetTargets( links, {
+    shouldDebug: window.DEBUG, shouldFilterLinks: true
+});
