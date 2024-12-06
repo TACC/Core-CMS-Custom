dbd92108557f5d818715d6fd203e23d089695502
Chandra Y <cyemparala@tacc.utexas.edu>
2024-12-05 21:37:04 -0600

    WP-796 - registration list needs to update on modal edits (#391)

--- apcd_cms/src/client/src/hooks/registrations/useForm.ts
+++ apcd_cms/src/client/src/hooks/registrations/useForm.ts
@@ -50,8 +50,15 @@ export function usePostRegistration() {
       console.log(err);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ['submitter-registrations', 'admin-registrations'],
+      const keysToInvalidate = [
+        'admin-registrations',
+        'submitter-registrations',
+      ];
+      keysToInvalidate.forEach((key) => {
+        queryClient.invalidateQueries({
+          queryKey: [key],
+          exact: false, // we do not know the parameters, so all keys are invalidated
+        });
       });
     },
   });
