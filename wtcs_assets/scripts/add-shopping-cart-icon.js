f1eb77bde31f95f18a23519399ca83dc0156d1a9
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-03-24 16:36:36 -0500

    refactor(wtcs): simplify new script

--- wtcs_assets/scripts/add-shopping-cart-icon.js
+++ wtcs_assets/scripts/add-shopping-cart-icon.js
@@ -1,22 +1,18 @@
-const icon = document.createElement('img');
-      icon.role = 'button';
-      icon.alt = 'Register';
-      icon.classList.add('icon');
-      icon.src = 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@feat/wtcs-shopping-cart-icon/wtcs_assets/icons/shopping-cart.png';
-      // icon.src = '../icons/shopping-cart.png';
+const alt = 'Register';
+const url = 'https://professionaled.utexas.edu/s/checkout-TACC';
+const icon = 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@feat/wtcs-shopping-cart-icon/wtcs_assets/icons/shopping-cart.png';
 
-const link = document.createElement('a');
-      link.href = 'https://professionaled.utexas.edu/s/checkout-TACC';
-      link.classList.add('nav-link');
-      link.target = '_blank';
-      link.appendChild(icon);
+// To mimic the login nav icon HTML, but as a shopping cart icon
+// https://github.com/TACC/Core-CMS/blob/v4.25.4/taccsite_cms/templates/nav_portal.html#L5-L10
+// https://github.com/TACC/Core-Portal/blob/v3.12.0/server/portal/templates/includes/nav_portal.raw.html#L53-L57
+const html = `
+  <ul class="navbar-nav s-portal-nav">
+    <li class="nav-item">
+      <a href="${url}" class="nav-link" target="_blank">
+        <img role="button" alt="${alt}" class="icon" src="${icon}">
+      </a>
+    </li>
+  </ul>
+`;
 
-const item = document.createElement('li');
-      item.classList.add('nav-item');
-      item.appendChild(link);
-
-const list = document.createElement('ul');
-      list.classList.add('navbar-nav', 's-portal-nav');
-      list.appendChild(item);
-
-document.getElementById('s-search-bar').parentElement.appendChild(list);
+document.getElementById('s-search-bar').parentElement.insertAdjacentHTML('beforeend', html);
