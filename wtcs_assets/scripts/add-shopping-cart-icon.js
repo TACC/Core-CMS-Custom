8f48d6dadeee8485be472efad803ced6399c799d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-03-24 16:23:33 -0500

    feat(wtcs): script to actually add linked icon
    Borrows HTML expected from portal nav.

--- wtcs_assets/scripts/add-shopping-cart-icon.js
+++ wtcs_assets/scripts/add-shopping-cart-icon.js
@@ -1,1 +1,22 @@
-// TODO: Add a shopping cart icon (../icons/shopping-cart.png) to the header
+const icon = document.createElement('img');
+      icon.role = 'button';
+      icon.alt = 'Register';
+      icon.classList.add('icon');
+      icon.src = 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS-Custom@feat/wtcs-shopping-cart-icon/wtcs_assets/icons/shopping-cart.png';
+      // icon.src = '../icons/shopping-cart.png';
+
+const link = document.createElement('a');
+      link.href = 'https://professionaled.utexas.edu/s/checkout-TACC';
+      link.classList.add('nav-link');
+      link.target = '_blank';
+      link.appendChild(icon);
+
+const item = document.createElement('li');
+      item.classList.add('nav-item');
+      item.appendChild(link);
+
+const list = document.createElement('ul');
+      list.classList.add('navbar-nav', 's-portal-nav');
+      list.appendChild(item);
+
+document.getElementById('s-search-bar').parentElement.appendChild(list);
