99810ac236f120b84792fd57abbf82327f5cdb0c
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-11-20 18:07:19 -0600

    feat(cfde-wheel): set prod env var in Vite config

--- cfde-wheel/vite.config.js
+++ cfde-wheel/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     lib: {
       entry: './index.js',
