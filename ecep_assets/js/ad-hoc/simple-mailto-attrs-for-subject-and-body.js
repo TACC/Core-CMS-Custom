579bddca8c3997758430ddd2724e843acc0fe97b
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-02-17 19:38:07 -0600

    fix(ecep): should use CDN link (not relative)

--- ecep_assets/js/ad-hoc/simple-mailto-attrs-for-subject-and-body.js
+++ ecep_assets/js/ad-hoc/simple-mailto-attrs-for-subject-and-body.js
@@ -1,6 +1,6 @@
 /* Add `subject` and `body` to `mailto:` links via attributes sans `data-` */
 /* https://github.com/TACC/Core-CMS/blob/v4.38.0/taccsite_cms/templates/assets_core_delayed.html#L12-L22 */
-import updateEmailLinkHrefs from '/static/site_cms/js/modules/updateEmailLinkHrefs.js';
+import updateEmailLinkHrefs from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@v4.38.0/taccsite_cms/static/site_cms/js/modules/updateEmailLinkHrefs.js';
 
 const scopeElement = document.getElementById('cms-content');
 const fakeText = '';
