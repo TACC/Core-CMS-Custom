e033e6c1da27ae18bb31f8ea8c29a8d27880adc6
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-05-21 18:06:05 -0500

    feat(txospo): ad-hoc JavaScript

---
+++ txospo/js/ad-hoc.js
@@ -0,0 +1,2 @@
+import 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@feat/force-external-articles-scripts/taccsite_cms/static/site_cms/js/modules/openAllArticlesAsExternal.js';
+import 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@feat/force-external-articles-scripts/taccsite_cms/static/site_cms/js/modules/redirectAllArticlesAsExternal.js';
