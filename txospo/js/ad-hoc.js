8cbc31f9b162b98aca4e2eea3b6d2b7c086986e9
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-05-21 20:03:57 -0500

    docs(txospo): save js-global-ad-hoc

--- txospo/js/ad-hoc.js
+++ txospo/js/ad-hoc.js
@@ -1,2 +1,5 @@
+/* To open all articles as if external articles */
+/* TODO: Once tested for "UI" for , comment these out */
+/* NOTE: If client will link to external instance (.e.g PDF), use these */
 import 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@feat/force-external-articles-scripts/taccsite_cms/static/site_cms/js/modules/openAllArticlesAsExternal.js';
 import 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@feat/force-external-articles-scripts/taccsite_cms/static/site_cms/js/modules/redirectAllArticlesAsExternal.js';
