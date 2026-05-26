2f58eba49d4292d4f800bd92822b23cbc856e162
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2026-05-26 13:06:51 -0500

    refactor(ptdatax): js-global-ad-hoc

---
+++ ptdatax_assets/js/open-certain-links-in-new-tab.js
@@ -0,0 +1,9 @@
+import findLinksAndSetTargets from 'https://cdn.jsdelivr.net/gh/TACC/Core-CMS@v4.39.2/taccsite_cms/static/site_cms/js/modules/setTargetForExternalLinks.js';
+
+const links = document.querySelectorAll(
+    'a[href*="/core/internal-docs/"]'
+);
+findLinksAndSetTargets( links, {
+    shouldDebug: false,
+    shouldFilter: false,
+});
